2af0a85103d5bdda6137ba53c6bf6484905c1468
Lorence Lim <lorence@crowdbotics.com>
2021-01-05 06:46:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/icy_sea_3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.icy_sea_3;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "icy_sea_3";
+    }
+}
